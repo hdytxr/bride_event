c1de22df4ad8865310b0529905ea8ed4c51c0858
hdytxr <rikyhidayat21@student.upi.edu>
2021-07-22 19:36:31 +0700

    add multer for upload photo, and morgan to logger middleware

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./routes");
 const { errorHandler } = require("./middlewares/errorHandler");
+const morgan = require('morgan')
 
 const app = express();
 const PORT = process.env.PORT;
@@ -10,6 +11,7 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'))
 app.use(router);
 app.use(errorHandler);
 
